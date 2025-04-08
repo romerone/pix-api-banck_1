20d68df07aa2d77073f42825df4be2105e5f22d1
romerone <romerone.ra@gmail.com>
2025-04-08 17:34:07 -0300

    Configuração correta para Python

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
+#!/bin/sh
+pip install -r requirements.txt && 
+uvicorn app.main:app --host 0.0.0.0 --port $PORT --timeout-keep-alive 30
